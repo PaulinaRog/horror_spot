4d20d1c3fd8fc3338c06384e3811055699c5231e
PaulinaRog <rog.paulina@gmail.com>
2023-02-09 18:25:47 +0100

    contact page - effect added

--- movie_reviews/src/views/Contact.jsx
+++ movie_reviews/src/views/Contact.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import Nav from "../components/Nav";
 
 export default function Contact() {
+  const [style, setStyle] = useState({ display: "none" });
+
+  const handleFocus = () => {
+    setStyle(null);
+  };
+
+  const handleBlur = () => {
+    setStyle({ display: "none" });
+  };
+
   return (
     <>
       <div className="contact">
@@ -26,13 +37,87 @@ export default function Contact() {
         </div>
         <form className="contact-form">
           <label className="contact-form-label">Imię:</label>
-          <input className="contact-form-input" />
+          <input
+            className="contact-form-input"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
           <label className="contact-form-label">E-mail:</label>
-          <input className="contact-form-input" />
+          <input
+            className="contact-form-input"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
           <label className="contact-form-label">Wiadomość:</label>
-          <textarea className="contact-form-input contact-form-textarea" />
+          <textarea
+            className="contact-form-input contact-form-textarea"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
           <button className="contact-form-send">wyślij</button>
         </form>
+        <div className="contact-spider" style={style && style}>
+          <div class="spider-web"></div>
+          <div class="spider-container">
+            <div class="arm-container right">
+              <div class="arm A">
+                <div class="arm B">
+                  <div class="arm C"></div>
+                </div>
+              </div>
+              <div class="arm A">
+                <div class="arm B">
+                  <div class="arm C"></div>
+                </div>
+              </div>
+              <div class="arm A">
+                <div class="arm B">
+                  <div class="arm C"></div>
+                </div>
+              </div>
+              <div class="arm A">
+                <div class="arm B">
+                  <div class="arm C"></div>
+                </div>
+              </div>
+            </div>
+            <div class="arm-container left">
+              <div class="arm A">
+                <div class="arm B">
+                  <div class="arm C"></div>
+                </div>
+              </div>
+              <div class="arm A">
+                <div class="arm B">
+                  <div class="arm C"></div>
+                </div>
+              </div>
+              <div class="arm A">
+                <div class="arm B">
+                  <div class="arm C"></div>
+                </div>
+              </div>
+              <div class="arm A">
+                <div class="arm B">
+                  <div class="arm C"></div>
+                </div>
+              </div>
+            </div>
+            <div class="spider-body">
+              <div class="eye eye-left"></div>
+              <div class="eye eye-right"></div>
+            </div>
+          </div>{" "}
+        </div>
+        {/* <div>
+          CREDIT
+          <a href="https://codepen.io/SofiaSergio/" target="_blank">
+            <div id="link">
+              <i class="fab fa-codepen"></i>
+              <p>watch other pens</p>
+            </div>
+          </a>
+        </div> */}
       </div>
     </>
   );
